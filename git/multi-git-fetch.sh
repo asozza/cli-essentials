2f4fc61d9d1350dac3fc54bb98f9187a91cf6327
asozza <asozza.ph@gmail.com>
2025-06-27 11:07:02 +0200

    git update

--- git/multi-git-fetch.sh
+++ git/multi-git-fetch.sh
@@ -4,5 +4,5 @@ find "/Users/asozza/Documents/github" -type d -name ".git" | while read repo; do
     repo_dir=$(dirname "$repo")
     echo "Fetching $repo_dir"
     git -C "$repo_dir" fetch
-    echo "-----------------------------"
+    echo "------------------------------------------------------"
 done
--- git/multi-git-status.sh
+++ git/multi-git-status.sh
@@ -4,5 +4,5 @@ find "/Users/asozza/Documents/github" -type d -name ".git" | while read repo; do
     repo_dir=$(dirname "$repo")
     echo "Checking $repo_dir"
     git -C "$repo_dir" status --short
-    echo "-----------------------------"
+    echo "------------------------------------------------------"
 done
