eb330f9ce3372391620b6da2b625a442004caa6b
asozza <asozza.ph@gmail.com>
2026-04-27 10:26:44 +0200

    generalize path for multi-git functions

--- git/multi-git-fetch.sh
+++ git/multi-git-fetch.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 
-find "/Users/asozza/Documents/github" -type d -name ".git" | while read repo; do
+CONFIG_FILE="config.txt"
+if [ -f "$CONFIG_FILE" ]; then
+    SEARCH_PATH=$(cat "$CONFIG_FILE" | xargs)
+else
+    echo "No config file found! "
+    exit 1
+fi
+
+#echo "Github main folder: $SEARCH_PATH"
+
+find "$SEARCH_PATH" -type d -name ".git" | while read repo; do
     repo_dir=$(dirname "$repo")
     echo "Fetching $repo_dir"
-    git -C "$repo_dir" fetch
+    git -C "$repo_dir" fetch --all --prune
     echo "------------------------------------------------------"
 done
--- git/multi-git-status.sh
+++ git/multi-git-status.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-find "/Users/asozza/Documents/github" -type d -name ".git" | while read repo; do
+CONFIG_FILE="config.txt"
+if [ -f "$CONFIG_FILE" ]; then
+    SEARCH_PATH=$(cat "$CONFIG_FILE" | xargs)
+else
+    echo "No config file found! "
+    exit 1
+fi
+
+
+find "$SEARCH_PATH" -type d -name ".git" | while read repo; do
     repo_dir=$(dirname "$repo")
     echo "Checking $repo_dir"
     git -C "$repo_dir" status --short
